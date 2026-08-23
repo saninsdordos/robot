8296fd27d3072aa7174c20840d63148c23cfefd0
saninsdordos <saninsasa93@gmail.com>
2026-08-23 04:38:46 +0300

    rt5

--- stream.py
+++ stream.py
@@ -5,6 +5,10 @@ from time import sleep
 from enum import Enum
 from enum import IntEnum
 
+START_BYTE = 0x5A
+UART_PORT = "/dev/ttyS5"
+BAUDRATE = 115200
+
 
 class commands(IntEnum):
     MOVE = 0x1
@@ -14,7 +18,7 @@ markers_id = [1, 2, 3]
 
 
 def send_command(command):
-    packet = bytes([ST, command])  # START  # example CRC
+    packet = bytes([90, command])  # START  # example CRC
 
     ser.write(packet)
 
@@ -24,6 +28,28 @@ def process_id(id):
         case 1:
             print("MOVE")
             send_command(commands.MOVE)
+
+
+def open_serial():
+
+    print(f"Opening UART5: {port}")
+
+    ser = serial.Serial(
+        port=UART_PORT,
+        baudrate=BAUDRATE,
+        # 8N1
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        # Prevent reads/writes from blocking forever
+        timeout=0.5,
+        write_timeout=0.5,
+        # UART5 pins only provide TX/RX,
+        # so disable all flow control.
+        xonxoff=False,
+        rtscts=False,
+        dsrdtr=False,
+    )
 
 
 aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
