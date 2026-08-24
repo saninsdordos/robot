ebfe42701f70ebf4320da46f6e94b7f911074b98
saninsdordos <saninsasa93@gmail.com>
2026-08-25 02:24:55 +0300

    rt86

--- esp32_firmware.py
+++ esp32_firmware.py
@@ -5,25 +5,31 @@ pwm = PWM(Pin(15), freq=50)
 
 uart0 = UART(1, baudrate=115200, tx=8, rx=3)
 
+
 def parsing_message():
-    received_message = []
+    received_message = b""
+
     while True:
         if uart0.any():
             message_byte = uart0.read(1)
-        if message_byte == b"*":
-            received_message = b""
-            continue
-        if message_byte == b"!":
-            break
 
-        received_message += message_byte
-        print(received_message)
-  if b"START" in received_message:
-    pwm.duty_ns(1500000)  # 1.5 ms pulse
-return true     
+            if message_byte == b"*":
+                received_message = b""
+                continue
+
+            if message_byte == b"!":
+                break
+
+            received_message += message_byte
+            print(received_message)
+
+    if b"START" in received_message:
+        pwm.duty_ns(1500000)  # 1.5 ms pulse
+        return True
+
+    return False
 
 
-
-def move() 
-  
-
+def move():
+    if parsing_message():
+        print("Moving...")
